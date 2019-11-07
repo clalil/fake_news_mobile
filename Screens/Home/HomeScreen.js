063068d0e5afd54bd163e355b50726fe54a6049e
clalil <cliljander@gmail.com>
2019-11-07 18:02:54 +0100

    Rewrite class component into functional hook. 500 error due to heroku empty atm.

--- Screens/Home/HomeScreen.js
+++ Screens/Home/HomeScreen.js
@@ -1,19 +1,32 @@
-import React, { Component } from "react"
-import { StyleSheet, Text, View } from "react-native"
-import { GetArticles } from "../../Services/ArticlesApiService"
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, FlatList } from "react-native"
+import { GetArticles } from '../../Services/ArticlesApiService'
 
-class HomeScreen extends Component {
-    state = {
-      articles: []
-    }
+const HomeScreen = () => {
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Fake News</Text>
-      </View>
-    );
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    let articles = GetArticles()
+    updateArticlesStateHandler(articles)
+  })
+
+  const updateArticlesStateHandler = articles => {
+    setArticles(articles)
   }
+
+  const displayArticle = () => {
+    articles.map((article) => {
+      return <FlatList key={article.id}><Text>{article.title}</Text><Text>{article.content}</Text></FlatList>
+    })
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Fake News</Text>
+      <Text>{articles ? displayArticle : "Loading"}</Text>
+    </View>
+  )
 }
 
 export default HomeScreen
