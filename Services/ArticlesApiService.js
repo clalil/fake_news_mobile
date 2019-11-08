7d93b9be3493a0b36a6080d79b2ad8294f9df1fd
clalil <cliljander@gmail.com>
2019-11-08 13:05:26 +0100

    Trying to debug, error change from 500 to 404 w Heroku.

--- Services/ArticlesApiService.js
+++ Services/ArticlesApiService.js
@@ -2,12 +2,14 @@ import axios from "axios"
 
 const url = `https://fake-news-api.herokuapp.com/`
 
-export const GetArticles = async () => {
+const GetArticles = async () => {
   try {
-    let response = await axios.get(url + "/v1/articles")
+    let response = await axios.get(url + "v1/articles")
     const articles = response.data
     return articles
   } catch (error) {
     console.error(error)
   }
 }
+
+export { GetArticles }
