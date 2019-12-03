6286f8d2c81967c637dda669cc4604c2226d1bbb
clalil <cliljander@gmail.com>
2019-12-03 19:02:21 +0100

    Add loginform component

---
+++ Screens/Home/LoginForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import React from 'react'
+
+const LoginForm = (props) => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default LoginForm
