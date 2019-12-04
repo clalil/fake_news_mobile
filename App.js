d50996bc6b184d453694df9f42922bc3e2d4eade
clalil <cliljander@gmail.com>
2019-12-04 20:16:54 +0100

    Clean upp App.js

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 import React from "react"
-import HomeScreen from "./Screens/Home/HomeScreen"
 import AppNavigator from './AppNavigator'
 
 export default class App extends React.Component {
   render() {
-    return (
-      <>
-    <AppNavigator />
-    <HomeScreen />
-    </>
-    )
+    return <AppNavigator />
   }
 }
--- Screens/Article/ArticleScreen.js
+++ Screens/Article/ArticleScreen.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React, { Component } from 'react'
 import { View } from "react-native"
 
-export default class ArticleScreen extends React.Component {
+export default class ArticleScreen extends Component {
   render() {
     return (
       <View>
